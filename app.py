6de8d2cd783db52ca384091a4a2d630b865863fb
Ahmer Ejaz <65377577+Ari-Ahmiii@users.noreply.github.com>
2022-06-03 18:45:36 +0500

    Flask Note app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= 'secret-key'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+
+#import after initilizing app
+from routes import *
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class AddTaskForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+
+class DeleteTaskForm(FlaskForm):
+    submit= SubmitField('Delete')
+
